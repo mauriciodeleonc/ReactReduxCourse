8ab249939a9e9459e37b99165fb57c74567a86f3
Mauricio De León <mauricio.deleon.c13@gmail.com>
2019-01-08 15:37:23 -0600

    Trying with dynamic content

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,7 +4,17 @@ import ReactDOM from 'react-dom';
 
 //Create react component
 const App = () => {
-  return <div>Hi There!</div>
+  const buttonText = 'Click Me';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText}</button>
+    </div>
+  );
 };
 
 //Take the react component and show it on the screen
